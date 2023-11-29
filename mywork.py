ec89dbae490c2b97d6cac872b33e24111b591d24
mmakamm <6340048522@student.chula.ac.th>
2023-11-29 17:45:25 +0700

    Update mywork.py

--- mywork.py
+++ mywork.py
@@ -2,3 +2,4 @@ import streamlit as st
 import pandas as pd
 st.header("Hello World 👏")
 st.write("This is my first app")
+input = st.text_input("Please add your OpenAI API key to continue", "you can get it from https://beta.openai.com/account/api-keys")
