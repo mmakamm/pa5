e382c65b4a4218697b400375b8ddfc2e71038ac0
mmakamm <6340048522@student.chula.ac.th>
2023-11-29 17:58:55 +0700

    Update mywork.py

--- mywork.py
+++ mywork.py
@@ -1,5 +1,5 @@
 import streamlit as st 
 import pandas as pd
-st.header("Caption Generator")
+st.header("รร")
 st.write("ไอเดียการเขียนแคปชั่นประกอบโพส โดยใช้ GPT-3 ของ OpenAI")
 user_input = st.sidebar.text_input("Please add your OpenAI API key to continue", "you can get it from https://beta.openai.com/account/api-keys")    
