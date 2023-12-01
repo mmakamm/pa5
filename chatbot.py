e5af2bc2dbb25d7002baab943fb5d5e25d47a5f7
mmakamm <101464298+mmakamm@users.noreply.github.com>
2023-12-01 14:42:28 +0700

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,9 +1,7 @@
 import openai
 import streamlit as st
+import pandas as pd
 import time
-
-# Replace OpenAI() with OpenAI(api_key='your_api_key') to initialize the client with the API key
-client = openai.openai(api_key='your_api_key')
 
 st.set_page_config(
     page_title="openai",
@@ -11,42 +9,37 @@ st.set_page_config(
     initial_sidebar_state='auto',
 )
 
-# Initialize session state
-if 'openai_apikey' not in st.session_state:
-    st.session_state.openai_apikey = ""
-if 'chatbot_input' not in st.session_state:
-    st.session_state.chatbot_input = ""
-
-st.header("Caption Generator")
-st.write("ไอเดียการเขียนแคปชั่นประกอบโพส")
-st.sidebar.header("Caption Generator")
+st.header("Inroduction Generator for blog post")
+st.write("ไอเดียการเขียนบทนำสำหรับบทความ")
+st.sidebar.header("Inroduction Generator")
 st.sidebar.text_input("Please add your OpenAI API key to continue", key='openai_apikey')
 
 if st.session_state.openai_apikey != "":
-    st.success('OpenAI API key provided!')
-    st.text_input("ต้องการเขียนแคปชั่นเกี่ยวกับอะไร", key="chatbot_input")
+    st.success('OpenAI API key provided!') #condition detect the invalid api_key
+    st.text_input("ต้องการเขียนบทนำเรื่องกับอะไร", key = "chatbot_input")
+
 
     openai.api_key = st.session_state.openai_apikey
-    prompt = f"Write 5 introductions for a blog post about {st.session_state.chatbot_input} and reason why I should use them"
-
-    response = None
+    prompt = f"Write 5 introductions for a blog post about {st.session_state.chatbot_input} and reason behind each introdution"
     def call_openai_api():
         try:
-            response = client.completions.create(
-                engine="text-davinci-002",
+            response = openai.Completion.create(
+                engine ="text-davinci-002",
                 prompt=prompt,
                 temperature=0.5,
                 max_tokens=3000
             )
             return response
-        except Exception as e:
-            st.error(f"Error occurred: {e}")
+        except openai.error.RateLimitError as e:
             print(f"Rate limit exceeded. Waiting before making the next request. Error: {e}")
             time.sleep(60)  # Wait for a minute
             return call_openai_api()
 
     response = call_openai_api()
     st.text_area("Response:", response.choices[0].text.strip())
+    df = pd.DataFrame(response.choices[0].text.strip().split("\n"), columns=['caption'])
+    df = df.dropna(axis=0)
+    st.write(df)
 
 else:
-    st.warning('Please enter your OpenAI API key!', icon='⚠️')
+    st.warning('Please enter your OpenAI API key!')
