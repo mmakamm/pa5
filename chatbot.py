d55a68b986af2835e654a5879c0f1812c79952ce
mmakamm <6340048522@student.chula.ac.th>
2023-12-01 12:09:42 +0700

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,24 +1,41 @@
 import openai
 import streamlit as st
 import pandas as pd
+import time
+
+st.set_page_config(
+    page_title="openai",
+    layout='wide',
+    initial_sidebar_state='auto',
+)
 
 st.header("Caption Generator")
+st.write("ไอเดียการเขียนแคปชั่นประกอบโพส")
 st.sidebar.header("Caption Generator")
-openai_api_key = st.sidebar.text_input("Please add your OpenAI API key to continue", "")
+st.sidebar.text_input("Please add your OpenAI API key to continue", key='openai_apikey')
 
-if openai_api_key:
-    st.sidebar.success('OpenAI API key provided!', icon='✅')
-else:
-    st.sidebar.warning('Please enter your OpenAI API key!', icon='⚠️')
+if st.session_state.openai_apikey != "":
+    st.success('OpenAI API key provided!', icon=':white_check_mark:') #condition detect the invalid api_key
+    st.text_input("ต้องการเขียนแคปชั่นเกี่ยวกับอะไร", key = "chatbot_input")
 
-chatbot_input = st.text_input("ต้องการเขียนแคปชั่นเกี่ยวกับอะไร")
 
-if openai_api_key and chatbot_input:
-    openai.api_key = openai_api_key
-    response = openai.Completion.create(
-      engine="text-davinci-002",
-      prompt=chatbot_input,
-      temperature=0.5,
-      max_tokens=100
-    )
+    openai.api_key = st.session_state.openai_apikey
+    prompt = f"Write 5 introductions for a blog post about {st.session_state.chatbot_input} and reason why I should use them"
+    def call_openai_api():
+        try:
+            response = openai.Completion.create(
+                engine ="text-davinci-002",
+                prompt=prompt,
+                temperature=0.5,
+                max_tokens=3000
+            )
+            return response
+        except openai.error.RateLimitError as e:
+            print(f"Rate limit exceeded. Waiting before making the next request. Error: {e}")
+            time.sleep(60)  # Wait for a minute
+            return call_openai_api()
+
+    response = call_openai_api()
     st.text_area("Response:", response.choices[0].text.strip())
+else:
+    st.warning('Please enter your OpenAI API key!', icon=':warning:')
