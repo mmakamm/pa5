f454afdbe9e363e6e8c47b32258980b10e709357
mmakamm <101464298+mmakamm@users.noreply.github.com>
2023-12-01 13:05:31 +0700

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -3,12 +3,13 @@ import streamlit as st
 import time
 from openai.types import Completion, CompletionChoice, CompletionUsage
 from openai import AsyncOpenAI
+client = AsyncOpenAI()
 
 st.header("Caption Generator")
 st.write("ไอเดียการเขียนแคปชั่นประกอบโพส")
 st.sidebar.header("Caption Generator")
 st.sidebar.text_input("Please add your OpenAI API key to continue", key='openai_apikey')
-client = AsyncOpenAI()
+
 if st.session_state.openai_apikey != "":
     st.success('OpenAI API key provided!')
     st.text_input("ต้องการเขียนแคปชั่นเกี่ยวกับอะไร", key="chatbot_input")
