0fa8bc3492c3c4c27d58de3023d4e3f8993130a8
mmakamm <6340048522@student.chula.ac.th>
2023-12-01 12:50:59 +0700

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,44 +1,6 @@
 import openai
 import streamlit as st
 
-class SessionState:
-    def __init__(self, **kwargs):
-        for key, val in kwargs.items():
-            setattr(self, key, val)
-
-
-# Function to get or create the session state
-def get_session_state():
-    session = get_report_ctx().session
-    if not hasattr(session, "_custom_session_state"):
-        session._custom_session_state = SessionState()
-    return session._custom_session_state
-
-st.set_page_config(
-    page_title="openai",
-    layout='wide',
-    initial_sidebar_state='auto',
-)
-
-# Initialize session state
-session_state = get_session_state()
-
-# Rest of your code remains unchanged
-# ...
-
-if session_state.openai_apikey != "":
-    st.set_page_config(
-    page_title="openai",
-    layout='wide',
-    initial_sidebar_state='auto',
-)
-
-# Initialize session state
-if 'openai_apikey' not in st.session_state:
-    st.session_state.openai_apikey = ""
-if 'chatbot_input' not in st.session_state:
-    st.session_state.chatbot_input = ""
-
 st.header("Caption Generator")
 st.write("ไอเดียการเขียนแคปชั่นประกอบโพส")
 st.sidebar.header("Caption Generator")
@@ -63,11 +25,6 @@ if st.session_state.openai_apikey != "":
             return response
         except Exception as e:
             st.error(f"Error occurred: {e}")
-
-    session_state.sync()  # Sync session state before calling the API
-    response = call_openai_api()
-    if response:
-        st.text_area("Response:", response.choices[0].text.strip())
 
 else:
     st.warning('Please enter your OpenAI API key!', icon='⚠️')
